f930ca04121bb878f19d447ca30fb17d0016bb0f
GreyMW <GreyMW@gmail.com>
2023-05-08 17:47:24 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className={"main-container"}>
+            <div className={"main-left-pane"}>
+                <p>hello</p>
+            </div>
+            <div className={"main-right-pane"}>
+
+            </div>
+        </div>
+    )
+}
+
+export default App
