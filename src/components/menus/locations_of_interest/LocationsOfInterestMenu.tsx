8380ca91ac6c578fe39b1a972244837080ff56f6
GreyMW <GreyMW@gmail.com>
2023-07-09 23:52:34 -0500

    Refactored LocationsOfInterestMenu.tsx

--- src/components/menus/locations_of_interest/LocationsOfInterestMenu.tsx
+++ src/components/menus/locations_of_interest/LocationsOfInterestMenu.tsx
@@ -4,41 +4,32 @@ import LocationOfInterest from "../../../logic/object_definitions/locationOfInte
 import MenuList from "../../../logic/object_definitions/menuList.ts";
 import Spacer from "../menu_subitems/Spacer.tsx";
 import LatLonZoomViewer from "../menu_subitems/LatLonZoomViewer.tsx";
+import ButtonSwitchMenu from "../menu_subitems/ButtonSwitchMenu.tsx";
 
 export default function LocationsOfInterestMenu({mapState}: { mapState: MapStateInterface }) {
-    function CreateLocationButton(location: LocationOfInterest, key: number) {
-
-        return (
-            <button key={key} className={'secondary-button'} onClick={
-                () => {
-                    mapState.setLocationOfInterest(location);
-                }}
-            >
-                {location.name}
-            </button>
-        )
-    }
-
-
 
     return (
         <div className={"main-menu-container"}>
             <ReturnToMain mapState={mapState}/>
             <Spacer/>
-
-            {mapState.locationsOfInterest.map((loc, index) => CreateLocationButton(loc, index))}
+            {mapState.locationsOfInterest.map((loc, index) => CreateLocationButton(loc, index, mapState))}
             <Spacer/>
-            <AddLocationOfInterest mapState={mapState}/>
+            <ButtonSwitchMenu mapState={mapState} menu={MenuList.add_location_of_interest} text={"Add Location of Interest"}/>
             <Spacer/>
             <LatLonZoomViewer mapState={mapState}/>
         </div>
     )
 }
 
-function AddLocationOfInterest({mapState}: { mapState: MapStateInterface }) {
+function CreateLocationButton(location: LocationOfInterest, key: number, mapState: MapStateInterface) {
+
     return (
-        <button className={'primary-button'}
-                onClick={() => mapState.setCurrentMenu(MenuList.add_location_of_interest)}>Add Location of
-            Interest</button>
+        <button key={key} className={'secondary-button'} onClick={
+            () => {
+                mapState.setLocationOfInterest(location);
+            }}
+        >
+            {location.name}
+        </button>
     )
 }
